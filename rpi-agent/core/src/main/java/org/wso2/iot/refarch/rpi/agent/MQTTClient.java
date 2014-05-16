d2fc1d868e61dbeaf19f1115232469e289da0d14
Afkham Azeez <azeez@wso2.com>
2014-05-16 17:25:31 +0530

    Added license headers

--- rpi-agent/core/src/main/java/org/wso2/iot/refarch/rpi/agent/MQTTClient.java
+++ rpi-agent/core/src/main/java/org/wso2/iot/refarch/rpi/agent/MQTTClient.java
@@ -1,31 +1,22 @@
-package org.wso2.iot.refarch.rpi.agent;
-
 /*
- * #%L
- * **********************************************************************
- * ORGANIZATION  :  WSO2
- * PROJECT       :  WSO2 IoT Reference Architecture - Raspberry Pi Agent
- * FILENAME      :  MQTTClient.java
- * 
- * This file is part of the Pi4J project. More information about
- * this project can be found here:  http://www.wso2.com/
- * **********************************************************************
- * %%
- * Copyright (C) 2014 WSO2
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- * 
- *      http://www.apache.org/licenses/LICENSE-2.0
- * 
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * #L%
+ *  Copyright (c) 2005-2014, WSO2 Inc. (http://www.wso2.org) All Rights Reserved.
+ *
+ *  WSO2 Inc. licenses this file to you under the Apache License,
+ *  Version 2.0 (the "License"); you may not use this file except
+ *  in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing,
+ *  software distributed under the License is distributed on an
+ *  "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ *  KIND, either express or implied.  See the License for the
+ *  specific language governing permissions and limitations
+ *  under the License.
+ *
  */
+package org.wso2.iot.refarch.rpi.agent;
 
 import org.eclipse.paho.client.mqttv3.*;
 import org.eclipse.paho.client.mqttv3.persist.MqttDefaultFilePersistence;
