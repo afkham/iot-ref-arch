c03d2b299fd56ac2035a2ddb596cf4f6b927f12d
Dulitha Wijewantha <dulithaz@gmail.com>
2014-05-17 00:42:56 +0530

    Removed humidity

--- rpi-agent/core/src/main/java/org/wso2/iot/refarch/rpi/agent/Publisher.java
+++ rpi-agent/core/src/main/java/org/wso2/iot/refarch/rpi/agent/Publisher.java
@@ -80,7 +80,7 @@ public class Publisher {
                 String message = "Temperature:" + Float.toString(temperature);
                 String humidityMsg = "Humidity:" + Integer.toString(humidity);
                 mqttClient.publish(1,message.getBytes());
-                mqttClient.publish(1,humidityMsg.getBytes());
+//                mqttClient.publish(1,humidityMsg.getBytes());
                 JSONObject infoObject = agent.createInfoObject();
                 //Inserting the sensors payload to info object
                 infoObject.put("sensors", payload);
